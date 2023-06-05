f6a0f53462cc8eb049fb3497c6612d4f469b3f42
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2023-06-05 10:10:54 +0200

    add Work component and bs-logo1 on assests file

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
+import BeachSpaceLogo from '../assets/images/bs-logo1.png'
 
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div>
+        <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-[#28bdec]'>Work</p>
           <p className='py-4' >// Check out some of my recent work</p>
         </div>
 
-        <div className='grid sm:grid-col-2 md:grid-col-3 gap-4 '>
-          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div style={{ backgroundImage: `url(${BeachSpaceLogo})` }} className='bg-white shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* Hover Effects */}
-            <div>
-              <span>
-
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-black tracking-wider'>
+                React JS Application
               </span>
-              <div>
+              <div className='pt-8 text-center'>
                 <a href="/">
-                  <button></button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
-
-                <a href="">
-                  <button></button>
+                <a href="/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
               </div>
             </div>

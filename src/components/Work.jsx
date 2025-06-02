bbad51357bb52a41bb289395ef5dd5e95ed28ecb
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2025-06-02 17:53:03 +0200

    add new work flex-col layout on Work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,10 +6,10 @@ import MicroActionsBlog from "../assets/images/micro-act-name-logo.svg";
 
 const Work = () => {
   const works = [
-    { label: 'Beach Space', href: 'https://github.com/diegocchaves/beach-space', url: 'https://beach-space.vercel.app/login', image: BeachSpaceImage },
-    { label: 'Cuidando do Money', href: 'https://github.com/diegocchaves/cuidando-do-money', url: 'https://cuidandodomoney.vercel.app/', image: CuidandoDoMoneyImage },
-    { label: 'Micro Actions', href: 'https://github.com/diegocchaves/micro-actions', url: '', image: MicroActionsImage },
     { label: 'Micro Actions Blog', href: 'https://www.microactions.blog/', url: '', image: MicroActionsBlog}, 
+    { label: 'Micro Actions', href: 'https://github.com/diegocchaves/micro-actions', url: '', image: MicroActionsImage },
+    { label: 'Cuidando do Money', href: 'https://github.com/diegocchaves/cuidando-do-money', url: 'https://cuidandodomoney.vercel.app/', image: CuidandoDoMoneyImage },
+    { label: 'Beach Space', href: 'https://github.com/diegocchaves/beach-space', url: 'https://beach-space.vercel.app/login', image: BeachSpaceImage },
   ];
 
   return (
@@ -20,29 +20,19 @@ const Work = () => {
           <p className='py-4'>// Check out some of my recent work</p>
         </div>
         {/* Container */}
-        <nav className='grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
+        <nav className='grid w-full h-full grid-cols-2 gap-4 p-4 md:flex md:flex-col '>
           {works.map((work, index) => (
             <div
               key={index}
-              className="relative shadow-lg bg-[#0e203b] shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div overflow-hidden"
+              className="flex items-center justify-start w-full h-full border border-gray-700 rounded-md md:py-3 md:px-3 "
             >
+              <div className='flex items-center justify-center border border-gray-700 rounded-md w-80 h-80 bg-gray-50'>
               {/* Background Image */}
               <img
                 src={work.image}
                 alt={work.label}
-                className="absolute items-center justify-center object-cover transition-all duration-300 rounded-md opacity-0 group-hover:opacity-100 group-hover:scale-95"
+                className="flex items-center justify-center w-40 h-40 p-2 m-2 transition-transform duration-300 ease-in-out rounded  md:w-fit md:h-fit"
               />
-
-              <div className='relative flex flex-col items-center justify-center'>
-                <p className='mb-12 text-xl font-bold'>{work.label}</p>
-                <div className='flex flex-row items-center justify-center gap-3'>
-                  <a href={work.href} className='py-4 text-2xl transition hover:text-purple-800'>
-                    <button className='px-4 py-2 m-2 text-sm font-medium text-center text-gray-700 bg-[#ccd6fde0] rounded-lg'>Code</button>
-                  </a>
-                  <a href={work.url} className='py-4 text-2xl transition hover:text-purple-800'>
-                    <button className='px-4 py-2 m-2 text-sm font-medium text-center text-gray-700 bg-[#ccd6fde0] rounded-lg'>Web</button>
-                  </a>
-                </div>
               </div>
             </div>
           ))}
@@ -57,3 +47,15 @@ export default Work;
 
 {/* <div className='opacity-0 group-hover:opacity-100'>
   </div> */}
+
+  // <div className='relative flex flex-col items-center justify-center'>
+  //               <p className='text-xl font-bold '>{work.label}</p>
+  //               {/* <div className='flex flex-row items-center justify-center gap-3'>
+  //                 <a href={work.href} className='py-4 text-2xl transition hover:text-purple-800'>
+  //                   <button className='px-4 py-2 m-2 text-sm font-medium text-center text-gray-700 bg-[#ccd6fde0] rounded-lg'>Code</button>
+  //                 </a>
+  //                 <a href={work.url} className='py-4 text-2xl transition hover:text-purple-800'>
+  //                   <button className='px-4 py-2 m-2 text-sm font-medium text-center text-gray-700 bg-[#ccd6fde0] rounded-lg'>Web</button>
+  //                 </a>
+  //               </div> */}
+  //             </div>

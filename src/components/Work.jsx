ff007a1324f1fa5046dc104e2b42a7799bbd23ea
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2025-06-05 13:49:04 +0200

    Add Date and Status on works array on Work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,9 +6,9 @@ import MicroActionsBlog from "../assets/images/ma-blog-mockup.svg";
 
 const Work = () => {
   const works = [
-    { label: 'Micro Actions Blog', href: 'https://github.com/diegocchaves/micro-actions-blog', url: 'https://www.microactions.blog/', image: MicroActionsBlog}, 
-    { label: 'Micro Actions', href: 'https://github.com/diegocchaves/micro-actions', url: '', image: MicroActionsImage },
-    { label: 'Cuidando do Money', href: 'https://github.com/diegocchaves/cuidando-do-money', url: 'https://cuidandodomoney.vercel.app/', image: CuidandoDoMoneyImage },
+    { label: 'Micro Actions Blog', href: 'https://github.com/diegocchaves/micro-actions-blog', url: 'https://www.microactions.blog', image: MicroActionsBlog, Date: 'Mon May 8 2023 ', status: 'In Progress' },
+    { label: 'Micro Actions', href: 'https://github.com/diegocchaves/micro-actions', url: 'https://www.microactions.co', image: MicroActionsImage },
+    { label: 'Cuidando do Money', href: 'https://github.com/diegocchaves/cuidando-do-money', url: 'https://cuidandodomoney.vercel.app', image: CuidandoDoMoneyImage },
     { label: 'Beach Space', href: 'https://github.com/diegocchaves/beach-space', url: 'https://beach-space.vercel.app/login', image: BeachSpaceImage },
   ];
 
@@ -34,18 +34,39 @@ const Work = () => {
                 className="flex items-center justify-center w-40 h-40 p-2 m-2 rounded md:w-full md:h-full md:p-2 md:m-0"
               />
               </div>
-              <div className='flex flex-col p-4 md:grid md:grid-cols-1 md:gap-4 md:items-center md:justify-center'>
+              <div className='flex flex-col p-8 md:grid md:grid-cols-1 md:gap-4 md:items-center md:justify-center'>
                 <div className='flex flex-col justify-start '>
                  <p className='text-xl font-bold'>{work.label}</p>
-                 <div className='flex flex-col gap-1 md:mt-5'>      
+                 <div className='flex flex-col gap-1 md:mt-5'>
                    <span className='font-medium text-gray-500'>Domain</span>
-                   <a href={work.href} className='text-sm font-medium text-white transition'>{work.href.replace(/https?:\/\//, '')}</a>
-                 
-                    <a href={work.url} className='text-sm font-medium text-gray-700 bg-[#ccd6fde0] rounded-lg hover:bg-gray-200 transition'>
-                      {work.url.replace(/https?:\/\//, '')}
-                    </a>
-               
+                   <a href={work.url} className='text-sm font-medium text-gray-100 transition hover:text-[#1da1f2]' target="_blank" rel="noopener noreferrer">{work.url.replace(/https?:\/\//, '')}</a>
+                   <span className='font-medium text-gray-500'>Source</span>
+                    <a href={work.href} className='text-sm font-medium text-gray-100 transition hover:text-[#1da1f2]' target="_blank" rel="noopener noreferrer"> {work.href.replace(/https?:\/\//, '')}</a>
+                    <div className='flex flex-row items-center justify-between mt-4'>
+                      <div className='flex flex-col text-gray-100'>
+                       <span className='mb-2 font-medium text-gray-500'>Status:</span>
+                        <span className='text-sm '>{work.status}</span>
+                      </div>
+                       <div className='flex flex-col text-gray-100'>
+                       <span className='mb-2 font-medium text-gray-500'>Created:</span>
+                        <span className='text-sm '>{work.Date}</span>
+                      </div>
+                    </div>
                   </div>
+                  {/* <div className='flex flex-col items-start justify-start mt-4'>
+                    <span className='text-gray-500'>Technologies:</span>
+                    <div className='flex flex-row items-center justify-start gap-2 mt-2'>
+                      <span className='text-sm font-medium text-gray-100'>React</span>
+                      <span className='text-sm font-medium text-gray-100'>Tailwind CSS</span>
+                      <span className='text-sm font-medium text-gray-100'>Node.js</span>
+                      <span className='text-sm font-medium text-gray-100'>Express</span>
+                    </div>
+                    <div className='flex flex-row items-center justify-start gap-2 mt-2'>
+                      <span className='text-sm font-medium text-gray-100'>MongoDB</span>
+                      <span className='text-sm font-medium text-gray-100'>Vercel</span>
+                      <span className='text-sm font-medium text-gray-100'>GitHub</span>
+                    </div>
+                  </div> */}
                 </div>
               </div>
             </div>
@@ -59,17 +80,3 @@ const Work = () => {
 export default Work;
 
 
-{/* <div className='opacity-0 group-hover:opacity-100'>
-  </div> */}
-
-  // <div className='relative flex flex-col items-center justify-center'>
-  //               <p className='text-xl font-bold '>{work.label}</p>
-  //               {/* <div className='flex flex-row items-center justify-center gap-3'>
-  //                 <a href={work.href} className='py-4 text-2xl transition hover:text-purple-800'>
-  //                   <button className='px-4 py-2 m-2 text-sm font-medium text-center text-gray-700 bg-[#ccd6fde0] rounded-lg'>Code</button>
-  //                 </a>
-  //                 <a href={work.url} className='py-4 text-2xl transition hover:text-purple-800'>
-  //                   <button className='px-4 py-2 m-2 text-sm font-medium text-center text-gray-700 bg-[#ccd6fde0] rounded-lg'>Web</button>
-  //                 </a>
-  //               </div> */}
-  //             </div>

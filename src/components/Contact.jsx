e4d8f062932bcf9f55ee069f43619a913ba1d56e
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2025-06-11 17:54:16 +0200

    Refactor Contact component for improved layout and styling consistency

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,20 +17,20 @@ const Contatc = () => {
   };
   
   return (
-    <div name='contact' className='flex items-center justify-center p-4 py-20 bg-black md:py-36' >
+    <div name='contact' className='flex flex-col items-center justify-center p-4 py-20 text-gray-300 bg-black md:py-36' >
+      <div className='flex flex-col text-center md:py-10 '>
+          <p className='inline text-lg font-bold md:text-xl'>[ Contact ]</p>
+          <p className='py-6 text-[15px] md:text-lg'>//Submit the form below or shoot me an email - diegocchaves21@gmail.com</p>
+      </div>
       <form method='POST'
        action="https://getform.io/f/f437e598-9e82-4608-8df9-dff71cc633c9"
-        className='flex flex-col max-w-[600px] w-full'
+        className='flex flex-col max-w-[600px] w-full border border-gray-700 p-4 md:p-8 rounded-lg text-sm  '
         onSubmit={handleSubmit}      
         autoComplete='off'
         >
-        <div className='flex flex-col items-center mb-8'>
-          <p className='inline text-lg font-bold text-gray-300 md:text-xl'>[ Contact ]</p>
-          <p className='py-4 text-gray-300 text-[15px] md:text-lg text-center '>//Submit the form below or shoot me an email - diegocchaves21@gmail.com</p>
-        </div>
-        <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' value={form.name} onChange={handleChange}  />
-        <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' value={form.email} onChange={handleChange} />
-        <textarea className='bg-[#ccd6f6] p-2' name='massage' rows='10' placeholder='Message' onChange={handleChange}></textarea>
+        <input className='p-2 bg-gray-800 border border-gray-600 rounded-md' type="text" placeholder='Name' name='name' value={form.name} onChange={handleChange}  />
+        <input className='p-2 my-4 bg-gray-800 border border-gray-600 rounded-md' type="email" placeholder='Email' name='email' value={form.email} onChange={handleChange} />
+        <textarea className='p-2 bg-gray-800 border border-gray-600 rounded-md' name='massage' rows='10' placeholder='Message' onChange={handleChange}></textarea>
         <button className='flex items-center px-4 py-3 mx-auto my-8 text-black bg-gray-100 border-2 rounded-md group hover:bg-gray-900 hover:border-gray-50 hover:text-white'>Let's Collaborate</button>
       </form>
     </div>

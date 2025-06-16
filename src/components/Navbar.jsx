60808e689a732d6d45416372565d9320ad1b2235
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2025-06-16 19:35:52 +0200

    Refactor Navbar component for improved readability and maintainability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,106 +1,151 @@
-import React, { useEffect, useState } from 'react'
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaRegFileAlt } from 'react-icons/fa'
+import React, { useEffect, useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaRegFileAlt,
+} from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import Logo from "../assets/images/Logo1.svg"
-import { Link } from 'react-scroll'
+import Logo from "../assets/images/Logo1.svg";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   useEffect(() => {
-     const handleResize = () => {
+    const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
-      window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
       return () => {
-        window.removeEventListener('resize', handleResize);
-      }
-  }}, [])
-  
+        window.removeEventListener("resize", handleResize);
+      };
+    };
+  }, []);
+
   // If not mobile, show all links
   const links = [
-    { label: '[', to: ""},
-    { label: 'Home', to: "home" },
-    { label: 'About', to: "about" },
-    { label: 'Skills', to: "skills" },
-    { label: 'Projects', to: "projects" },
-    { label: 'Contributions', to: "contributions" },
-    { label: 'Contact', to: "contact" },
-    { label: ']', to: "" },
-  ]
-  
+    { label: "[", to: "" },
+    { label: "Home", to: "home" },
+    { label: "About", to: "about" },
+    { label: "Skills", to: "skills" },
+    { label: "Projects", to: "projects" },
+    { label: "Contributions", to: "contributions" },
+    { label: "Contact", to: "contact" },
+    { label: "]", to: "" },
+  ];
+
   // Filter out [ and ] on mobile
-   const filteredLinks = isMobile ? links.filter(link => link.label !== '[' && link.label !== ']' ): links;
+  const filteredLinks = isMobile
+    ? links.filter((link) => link.label !== "[" && link.label !== "]")
+    : links;
 
   const icons = [
-    { icon: <FaLinkedin size={18} />, label: 'Linkedin', href: 'https://www.linkedin.com/in/diego-carve/'},
-    { icon: <FaGithub size={18} />, label: 'Github', href: 'https://github.com/Diegocchaves'},
-    { icon: <FaXTwitter size={18} />, label: 'Twitter/X', href: 'https://twitter.com/diego_carve' },
-    { icon: <FaRegFileAlt size={18} />, label: 'Resume', href: 'https://drive.google.com/file/d/1qfcAZMTdLYSH-7sobEz7jEOqdmkLLnMB/view?usp=drive_link' },
-  ]
+    {
+      icon: <FaLinkedin size={18} />,
+      label: "Linkedin",
+      href: "https://www.linkedin.com/in/diego-carve/",
+    },
+    {
+      icon: <FaGithub size={18} />,
+      label: "Github",
+      href: "https://github.com/Diegocchaves",
+    },
+    {
+      icon: <FaXTwitter size={18} />,
+      label: "Twitter/X",
+      href: "https://twitter.com/diego_carve",
+    },
+    {
+      icon: <FaRegFileAlt size={18} />,
+      label: "Resume",
+      href: "https://drive.google.com/file/d/1qfcAZMTdLYSH-7sobEz7jEOqdmkLLnMB/view?usp=drive_link",
+    },
+  ];
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-6 bg-black text-gray-30 z-10 '>
-      <div className='flex items-center justify-start '>
-        <Link to='home' smooth={true} duration={500} className='flex justify-start cursor-pointer'>
-          <img src={Logo} alt='logo' className='w-8 h-8' />
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-6 bg-black text-gray-30 z-10 ">
+      <div className="flex items-center justify-start ">
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          className="flex justify-start cursor-pointer"
+        >
+          <img src={Logo} alt="logo" className="w-8 h-8" />
         </Link>
       </div>
 
       {/* menu */}
-      <nav className='hidden gap-2 md:flex'>
+      <nav className="hidden gap-2 md:flex">
         {links.map((link, index) => (
-          <div key={index} >
-            <Link to={link.to}
+          <div key={index}>
+            <Link
+              to={link.to}
               smooth={true}
               duration={500}
-              className='cursor-pointer text-white text-sm transition hover:text-[#1da1f2] hover:font-bold font-extralight '
-              activeClass='active-link'
-              spy={true}>
+              className="cursor-pointer text-white text-sm transition hover:text-[#1da1f2] font-extralight "
+              activeClass="active-link"
+              spy={true}
+            >
               {link.label}
             </Link>
           </div>
-        )
-        )}
+        ))}
       </nav>
 
       {/* hamburge */}
-      <div onClick={handleClick} className='z-10 text-white md:hidden'>
+      <div onClick={handleClick} className="z-10 text-white md:hidden">
         {!nav ? <FaBars size={20} /> : <FaTimes size={10} />}
       </div>
 
       {/* mobile menu */}
-      <nav className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center gap-10'}>
+      <nav
+        className={
+          !nav
+            ? "hidden"
+            : " absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center gap-10"
+        }
+      >
         {filteredLinks.map((link, index) => (
-          <Link key={index}
+          <Link
+            key={index}
             to={link.to}
             smooth={true}
             duration={500}
             onClick={() => setNav(false)}
-            className='items-center justify-center text-xl text-white'>
+            className="items-center justify-center text-xl text-white"
+          >
             <p>{link.label}</p>
           </Link>
         ))}
       </nav>
 
       {/* social icons */}
-      <nav className='hidden md:flex fixed flex-col top-[35%] gap-4 left-0 justify-center z-10'>
+      <nav className="hidden md:flex fixed flex-col top-[35%] gap-4 left-0 justify-center z-10">
         {icons.map((icon, index) => (
-          <div key={index} className={`w-[150px] h-[60px] flex justify-between items-center ml-[-96px] hover:ml-[-10px] duration-300 ${icon.color}`}>
+          <div
+            key={index}
+            className={`w-[150px] h-[60px] flex justify-between items-center ml-[-96px] hover:ml-[-10px] duration-300 ${icon.color}`}
+          >
             <a
               href={icon.href}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='flex items-center justify-around w-full gap-4 text-gray-300'>
-              <p className='flex flex-col items-center justify-center'>{icon.label}</p>
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-around w-full gap-4 text-gray-300"
+            >
+              <p className="flex flex-col items-center justify-center">
+                {icon.label}
+              </p>
               {icon.icon}
             </a>
           </div>
         ))}
       </nav>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
